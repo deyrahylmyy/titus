46b59fd31f488337ca281d78d55668d292272589
asus indonesia <dracreativity91@gmail.com>
2023-02-20 21:15:34 +0700

    update

---
+++ admin-dashboard/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MdHeight } from 'react-icons/md';
+
+const Footer = () => (
+  <div className="mt-24">
+    {/* <p className="dark:text-gray-200 text-gray-700 text-center m-96">
+      COMING SOON 
+    </p> */}
+    <div>
+      <h1 style={{color: "red"}}>Hello Style!</h1>
+      <button>New Article</button>
+      <br />
+      <button>Refresh</button>
+      <form action="submit">
+        <input type="text" name="" id="" />
+      </form>
+      </div>
+  </div>
+);
+
+export default Footer;
